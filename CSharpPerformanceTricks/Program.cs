83cec55b4ae1bd816e5602f514c842810b3edd63
Takuo Yoneda <shakeweb@gmail.com>
2017-09-29 18:49:55 +0900

    the first commit

---
+++ CSharpPerformanceTricks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharpPerformanceTricks
+{
+	class MainClass
+	{
+		private static void DrawLine(int x1, int y1, int x2, int y2)
+		{
+			// TODO: put line drawing code here
+		}
+
+		private static void DrawSquare(int x, int y, int w, int h)
+		{
+			int xw = x + w; 
+			int yh = y + h;
+
+			DrawLine (x, y, xw, y);
+			DrawLine (xw, y, xw, yh);
+			DrawLine (xw, yh, x, yh);
+			DrawLine (x, yh, x, y);
+		}
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Going to draw a square...");
+			DrawSquare (100, 100, 50, 50);
+			Console.WriteLine("All done!");
+		}
+	}
+}
